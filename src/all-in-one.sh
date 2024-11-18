657f51e951b83a465b0ee68c0705ba679106ad92
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-18 18:10:44 -0300

    Updating app files

---
+++ src/all-in-one.sh
@@ -0,0 +1,25 @@
+. ./src/apikey.sh
+
+. ./src/model_id.sh
+
+previous_text=""
+
+while read text
+
+do
+
+[ -z "${text}" ] && continue
+
+echo "text: ${text}; ${#text}"
+
+curl -isSL -X POST "https://api.elevenlabs.io/v1/text-to-speech/hwnuNyWkl9DjdTFykrN6" \
+ --header "xi-api-key: ${apikey}" \
+ --header 'Content-Type: application/json' \
+ --data "{ \"text\": \"${text}\" , \"model_id\": \"${model_id}\" , \"previous_text\": \"${previous_text}\",  \"language_code\": \"pt\" }" \
+ --output "${text}.mp4"
+
+previous_text="${previous_text} ${text}"
+
+echo "previous_text: ${#previous_text}"
+
+done < filename.txt
